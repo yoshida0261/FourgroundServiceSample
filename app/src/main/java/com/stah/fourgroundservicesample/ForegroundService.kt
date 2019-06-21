ab317bbcf6af37419ea2233c35c2370137cdb7f2
yoshida0261 <yoshida261@gmail.com>
2019-06-21 15:54:03 +0900

    init

---
+++ app/src/main/java/com/stah/fourgroundservicesample/ForegroundService.kt
@@ -0,0 +1,12 @@
+package com.stah.fourgroundservicesample
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+
+class ForegroundService: Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
