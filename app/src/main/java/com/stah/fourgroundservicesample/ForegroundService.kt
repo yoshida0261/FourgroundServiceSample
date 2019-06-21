771729e2924048b6ee4078dadf6c4210545a92df
yoshida0261 <yoshida261@gmail.com>
2019-06-21 17:19:14 +0900

    feat: add foreGroundService

--- app/src/main/java/com/stah/fourgroundservicesample/ForegroundService.kt
+++ app/src/main/java/com/stah/fourgroundservicesample/ForegroundService.kt
@@ -1,12 +1,80 @@
 package com.stah.fourgroundservicesample
 
+import android.R
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 
 
-class ForegroundService: Service() {
+class ForegroundService : Service() {
     override fun onBind(intent: Intent?): IBinder? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        createNotificationChannel()
+        println("service oncreate")
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        println("service onStartCommand")
+
+
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, notificationIntent, 0
+        )
+
+        val notification = NotificationCompat.Builder(this, "test")
+            .setContentTitle("Foreground Service")
+            .setContentText("Foreground Service Example in Android")
+            .setSmallIcon(R.drawable.sym_def_app_icon)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        startForeground(1, notification)
+
+
+        Thread(
+            Runnable {
+                (0..5).map {
+                    Thread.sleep(1000)
+                    println("foreground service $it")
+                }
+                stopForeground(true)
+                stopSelf()
+
+            }).start()
+
+
+        return START_STICKY
+
+    }
+
+    override fun onDestroy() {
+        println("service done")
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                "test",
+                "Foreground Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(serviceChannel)
+        }
     }
 }
--- app/src/main/java/com/stah/fourgroundservicesample/MainActivity.kt
+++ app/src/main/java/com/stah/fourgroundservicesample/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.stah.fourgroundservicesample
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
+
+import android.app.Notification
+import android.app.PendingIntent
+import androidx.core.app.NotificationCompat
+
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        buttonService.setOnClickListener {
+            println("button on")
+
+
+
+
+
+            val serviceIntent = Intent(this, ForegroundService::class.java)
+            startForegroundService(serviceIntent)
+
+        }
+
     }
 }
